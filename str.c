436e739424a4d9cd83197e6fff3284c272f6c6ae
Dan Nicholson <dbn.lists@gmail.com>
2008-02-08 16:15:28 -0800

    Check for errors in memory allocation functions

--- str.c
+++ str.c
@@ -6,10 +6,19 @@ dstring_t *d_string_new(const char *init)
 	dstring_t *new;
 
 	new = malloc(sizeof(dstring_t));
-	if (!new)
+	if (!new) {
+		perror(__FUNCTION__);
 		goto out;
+	}
 
 	new->str = strdup(init);
+	if (!new->str) {
+		perror(__FUNCTION__);
+		free(new);
+		new = NULL;
+		goto out;
+	}
+
 	new->len = strlen(new->str);
 out:
 	return new;
--- test.c
+++ test.c
@@ -4,9 +4,9 @@
 int main(int argc, char *argv[])
 {
 	dstring_t *foo = d_string_new("hello world");
-	if (foo) {
-		printf("String '%s' has '%d' characters\n",
-			foo->str, foo->len);
-	}
+	if (!foo)
+		return 1;
+
+	printf("String '%s' has '%d' characters\n", foo->str, foo->len);
 	return 0;
 }
