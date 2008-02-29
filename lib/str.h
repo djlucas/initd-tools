d5c9c9cdcc7a2b19dbb2df9548e59ae467b86723
Dan Nicholson <dbn.lists@gmail.com>
2008-02-28 20:57:15 -0800

    Add a strarg_t type for generic strargs
    Whereas the dep_t has a specific purpose with likeminded member name,
    there are also times when a generic strarg is useful. This adds a type
    for these situations, strarg_t.

--- lib/str.h
+++ lib/str.h
@@ -5,6 +5,11 @@ extern char *d_string_new(const char *new);
 extern void d_string_free(char *str);
 extern char *d_string_append(char *ds, const char *extra);
 
+typedef struct strarg {
+	unsigned int nstr;
+	char **str;
+} strarg_t;
+
 extern char **strarg_new(unsigned int *num);
 extern void strarg_free(char **strarg, unsigned int num);
 extern char **strarg_add(char **strarg, unsigned int *num, const char *s);
