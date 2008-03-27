d55f0b7020724189b2b290abb5ee3bbb03b21117
Dan Nicholson <dbn.lists@gmail.com>
2008-03-27 08:42:17 -0700

    Add dep_exists macro mapping to strarg_exists

--- lib/dep.h
+++ lib/dep.h
@@ -12,5 +12,6 @@ extern dep_t *dep_new(void);
 extern void dep_free(dep_t *dp);
 extern void dep_add(dep_t *dp, const char *name);
 extern dep_t *dep_copy(dep_t *source);
+#define dep_exists(dp, name) strarg_exists(dp->dep, dp->ndep, name)
 
 #endif /* _dep_h_ */
