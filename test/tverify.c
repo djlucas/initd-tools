89b6784a649b4f111da3c04c358fa8952fd101aa
Dan Nicholson <dbn.lists@gmail.com>
2008-04-29 19:07:32 -0700

    New test exercising parsing and dep checking
    Added a new test, tverify, that combines the parsing and collection of
    scripts in the init.d directory with checking the deps of script. This
    pretty much simulates the first half of install_initd. The missing part,
    of course, is the actual creation of the links.

---
+++ test/tverify.c
@@ -0,0 +1,73 @@
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <stdio.h>
+#include "initd.h"
+#include "types.h"
+#include "rdep.h"
+
+static void print_sk_list(const initd_list_t *list, initd_sk_t sk);
+
+int main(int argc, char *argv[])
+{
+	initd_list_t *all, *startlist, *stoplist;
+	dep_t *need = dep_new();
+
+	if (argc <= 1)
+		dep_add(need, "foo");
+
+	while (argc > 1) {
+		dep_add(need, argv[1]);
+		argv++;
+		argc--;
+	}
+
+	all = initd_list_from_dir("init.d");
+	initd_list_set_actives(all, "init.d");
+
+	initd_recurse_set_verbose(true);
+	startlist = initd_recurse_deps(all, RC_START, need);
+	if (startlist) {
+		print_sk_list(startlist, RC_START);
+	} else {
+		fprintf(stderr, "Failed recursing start scripts\n");
+		return 1;
+	}
+
+	stoplist = initd_recurse_deps(all, RC_STOP, need);
+	if (stoplist) {
+		print_sk_list(stoplist, RC_STOP);
+	} else {
+		fprintf(stderr, "Failed recursing stop scripts\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+static void print_sk_list(const initd_list_t *list, initd_sk_t sk)
+{
+	char *startstop;
+	initd_t *ip;
+
+	switch (sk) {
+	case RC_START:
+		startstop = "start";
+		break;
+	case RC_STOP:
+		startstop = "stop";
+		break;
+	default:
+		startstop = "";
+	}
+
+	if (list) {
+		printf("All deps for %s\n", startstop);
+		printf("Ordered:");
+		for (ip = list->first; ip; ip = ip->next)
+			printf(" %s", ip->name);
+		printf("\n");
+	} else {
+		printf("%s list is empty\n", startstop);
+	}
+}
